6f37cf5dfe09b5632bef313f56de7831f6958ac0
Mayank Goyal <mayankgoyal.mait@gmail.com>
2022-03-13 19:16:27 +0530

    Model Added - express validation

---
+++ models/User.js
@@ -0,0 +1,31 @@
+/** @format */
+
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  avatar: {
+    type: String,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+// gravatar... atachah profile Image to email. You may have one without relizing.
+// Always accessible in user model
+
+module.exports = User = mongoose.model('user', UserSchema);
